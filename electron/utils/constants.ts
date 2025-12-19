d59e41cb245487ead79eab1979e6ce1b88e1003d
Lingikaushikreddy <Kaushikreddylingireddy2002@gmail.com>
2025-12-19 00:13:05 -0500

    feat(utils): add global constants

---
+++ electron/utils/constants.ts
@@ -0,0 +1,5 @@
+
+export const APP_NAME = 'Savvy AI';
+export const VERSION = '1.0.0';
+export const SUPPORTED_CRMS = ['salesforce', 'hubspot', 'pipedrive'];
+export const AUDIO_SAMPLE_RATE = 16000;
